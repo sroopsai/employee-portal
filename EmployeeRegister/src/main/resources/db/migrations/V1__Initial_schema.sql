5acda26796bf71b801fb5287a34ed8f2de34fe80
roopsai <roop.sai.surampudi@proton.me>
2023-12-20 10:18:25 +0530

    Add schema.sql file

--- EmployeeRegister/src/main/resources/db/migrations/V1__Initial_schema.sql
+++
@@ -1,8 +0,0 @@
-create table employee (
-    id bigserial NOT NULL,
-    created_date timestamp not null,
-    last_modified_date timestamp not null,
-    name varchar(255) NOT NULL,
-    designation varchar(255) NOT NULL,
-    version integer NOT NULL
-)
